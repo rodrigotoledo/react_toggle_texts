c3890631e6f97f92191d51f5736fb7cbdb1999fb
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2021-10-02 18:13:56 -0300

    #toggle its ok - finish arrow function that make toggable values

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+	const [textOne, setTextOne] = useState("");
+	const [textTwo, setTextTwo] = useState("");
+	let toggleTexts = (e:any) => {
+		const tmpTextOne = textOne;
+		const tmpTextTwo = textTwo;
+		setTextTwo(tmpTextOne)
+		setTextOne(tmpTextTwo)
+		e.preventDefault()
+	}
+
   return (
     <form>
       <h1>Toggable</h1>
-      <h3>Fill the fields and press,<br /> magic will happens</h3>
+      <h3>Fill the fields and Click<br /> magic will happens</h3>
 			<fieldset>
 				<label htmlFor="textOne">Text One</label>
-				<textarea placeholder="Togglable text One" id="textOne"></textarea>
+				<textarea placeholder="Togglable text One" value={textOne} onChange={(event) => { setTextOne(event.target.value) }}></textarea>
 			</fieldset>
-			<button>Click for Toggle Texts</button>
+			<button onClick={toggleTexts}>Click for Toggle Texts</button>
 			<fieldset>
 				<label htmlFor="textTwo">Text Two</label>
-				<textarea placeholder="Togglable text Two" id="textTwo"></textarea>
+				<textarea placeholder="Togglable text Two" value={textTwo} onChange={(event) => { setTextTwo(event.target.value) }}></textarea>
 			</fieldset>
     </form>
   );
